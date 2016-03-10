312bb9b405a6d4d3d14abc1216686e701bd82e0b
Brandon B <bmbost1983@gmail.com>
2016-03-10 00:28:49 -0600

    Login Page
    Created a simple login page in HTML, and started work on the php login.
    Will finish tomorrow or Friday.

---
+++ Original-Website/public_html/login/admin.php
@@ -0,0 +1,8 @@
+<?php
+$dbHostname = 'localhost';
+$dbUsername = 'brandon';
+$dbPassword = 'bost';
+$dbName = 'athens911';
+
+$connect = mysqli_connect($dbHostname, $dbUsername, $dbPassword, $dbName)
+  or die("Unable to connect to MySQL: " . mysqli_error());
